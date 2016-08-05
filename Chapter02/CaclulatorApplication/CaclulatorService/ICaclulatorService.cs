25e487238b2760474b2f70e86340d89482ca4766
tsushi <tsushi@DESKTOP-MC5LH8Q>
2016-08-06 05:45:11 +0900

    Create a calculator stateless service

---
+++ Chapter02/CaclulatorApplication/CaclulatorService/ICaclulatorService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Remoting;
+
+namespace CaclulatorService
+{
+    interface ICaclulatorService: IService
+    {
+        Task<int> Add(int a, int b);
+        Task<int> Subtract(int a, int b);
+    }
+}
