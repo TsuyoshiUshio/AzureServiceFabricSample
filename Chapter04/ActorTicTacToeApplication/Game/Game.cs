cd5e676ec0384b17b3313f17875bb6cfe788322b
tsushi <tsushi@DESKTOP-MC5LH8Q>
2016-08-07 17:35:37 +0900

    Initial state of the Actor Tic Tac Toe

---
+++ Chapter04/ActorTicTacToeApplication/Game/Game.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Runtime;
+using Microsoft.ServiceFabric.Actors.Client;
+using Game.Interfaces;
+using System.Runtime.Serialization;
+
+namespace Game
+{
+    /// <remarks>
+    /// This class represents an actor.
+    /// Every ActorID maps to an instance of this class.
+    /// The StatePersistence attribute determines persistence and replication of actor state:
+    ///  - Persisted: State is written to disk and replicated.
+    ///  - Volatile: State is kept in memory only and replicated.
+    ///  - None: State is kept in memory only and not replicated.
+    /// </remarks>
+    [StatePersistence(StatePersistence.Persisted)]
+    internal class Game : Actor, IGame
+    {
+
+
+        /// <summary>
+        /// This method is called whenever an actor is activated.
+        /// An actor is activated the first time any of its methods are invoked.
+        /// </summary>
+        protected override Task OnActivateAsync()
+        {
+            ActorEventSource.Current.ActorMessage(this, "Actor activated.");
+
+            // The StateManager is this actor's private state store.
+            // Data stored in the StateManager will be replicated for high-availability for actors that use volatile or persisted state storage.
+            // Any serializable object can be saved in the StateManager.
+            // For more information, see http://aka.ms/servicefabricactorsstateserialization
+
+            return this.StateManager.TryAddStateAsync("count", 0);
+        }
+
+        public Task<bool> JoinGameAsync(long playerId, string playerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int[]> GetGameBoardAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetWinnerAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> MakeMoveAsync(long playerId, int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
