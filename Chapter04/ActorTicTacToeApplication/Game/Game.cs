51892a0f59b69c8e7ee15529ba55490a607fc7e4
tsushi <tsushi@DESKTOP-MC5LH8Q>
2016-08-07 20:34:25 +0900

    Adding TicTacToe feature

--- Chapter04/ActorTicTacToeApplication/Game/Game.cs
+++ Chapter04/ActorTicTacToeApplication/Game/Game.cs
@@ -8,6 +8,8 @@ using Microsoft.ServiceFabric.Actors.Runtime;
 using Microsoft.ServiceFabric.Actors.Client;
 using Game.Interfaces;
 using System.Runtime.Serialization;
+using Microsoft.ServiceFabric.Data;
+using System.ComponentModel;
 
 namespace Game
 {
@@ -19,10 +21,23 @@ namespace Game
     ///  - Volatile: State is kept in memory only and replicated.
     ///  - None: State is kept in memory only and not replicated.
     /// </remarks>
-    [StatePersistence(StatePersistence.Persisted)]
+    [StatePersistence(StatePersistence.Volatile)]
     internal class Game : Actor, IGame
     {
-
+        [DataContract]
+        public class ActorState
+        {
+            [DataMember]
+            public int[] Board;
+            [DataMember]
+            public string Winner;
+            [DataMember]
+            public List<Tuple<long, string>> Players;
+            [DataMember]
+            public int NextPlayerIndex;
+            [DataMember]
+            public int NumberOfMoves;
+        }
 
         /// <summary>
         /// This method is called whenever an actor is activated.
@@ -30,34 +45,104 @@ namespace Game
         /// </summary>
         protected override Task OnActivateAsync()
         {
-            ActorEventSource.Current.ActorMessage(this, "Actor activated.");
+            ConditionalValue<ActorState> state = this.StateManager.TryGetStateAsync<ActorState>("MyState").GetAwaiter().GetResult();
+            if (!state.HasValue)
+            {
+                var actorState = new ActorState()
+                {
+                    Board = new int[9],
+                    Winner = "",
+                    Players = new List<Tuple<long, string>>(),
+                    NextPlayerIndex = 0,
+                    NumberOfMoves = 0
+                };
+                this.StateManager.SetStateAsync<ActorState>("MyState", actorState);
+            }
+            return Task.FromResult(true);
+        }
+        
+        private ActorState State
+        {
+            get
+            {
+                return this.StateManager.TryGetStateAsync<ActorState>("MyState").GetAwaiter().GetResult().Value;
+            }
 
-            // The StateManager is this actor's private state store.
-            // Data stored in the StateManager will be replicated for high-availability for actors that use volatile or persisted state storage.
-            // Any serializable object can be saved in the StateManager.
-            // For more information, see http://aka.ms/servicefabricactorsstateserialization
-
-            return this.StateManager.TryAddStateAsync("count", 0);
+            set
+            {
+                this.StateManager.AddOrUpdateStateAsync<ActorState>("MyState", value, (ke, v) => value);
+            }
+            
         }
 
         public Task<bool> JoinGameAsync(long playerId, string playerName)
         {
-            throw new NotImplementedException();
+            var state = this.State;
+           if (state.Players.Count >= 2 || state.Players.FirstOrDefault(p => p.Item2 == playerName) != null)
+            {
+                return Task.FromResult<bool>(false);
+            }
+
+            state.Players.Add(new Tuple<long, string>(playerId, playerName));
+            this.State = state;
+            return Task.FromResult<bool>(true);
         }
 
+        [ReadOnly(true)]
         public Task<int[]> GetGameBoardAsync()
         {
-            throw new NotImplementedException();
+            return Task.FromResult<int[]>(this.State.Board);
         }
-
+        [ReadOnly(true)]
         public Task<string> GetWinnerAsync()
         {
-            throw new NotImplementedException();
+            return Task.FromResult<string>(this.State.Winner);
         }
 
         public Task<bool> MakeMoveAsync(long playerId, int x, int y)
         {
-            throw new NotImplementedException();
+            var state = this.State;
+            if (x < 0 || x > 2 || y < 0 || y > 2
+                || state.Players.Count != 2
+                || state.NumberOfMoves >= 9
+                || state.Winner != "")
+                return Task.FromResult<bool>(false);
+
+            int index = state.Players.FindIndex(p => p.Item1 == playerId);
+            if (index == state.NextPlayerIndex)
+            {
+                if (state.Board[y * 3 + x] == 0)
+                {
+                    int piece = index * 2 - 1;
+                    state.Board[y * 3 + x] = piece;
+                    state.NumberOfMoves++;
+                    if (HasWon(piece * 3))
+                        state.Winner = state.Players[index].Item2 + " (" +
+                            (piece == -1 ? "X" : "0") + ")";
+                    else if (state.Winner == "" && state.NumberOfMoves >= 9)
+                        state.Winner = "TIE";
+                    state.NextPlayerIndex = (state.NextPlayerIndex + 1) % 2;
+                    this.State = state;
+                    return Task.FromResult<bool>(true);
+                }
+                else
+                    return Task.FromResult<bool>(false);
+            }
+            else
+                return Task.FromResult<bool>(false);
+        }
+
+        private bool HasWon(int sum)
+        {
+            var state = this.State;
+            return state.Board[0] + state.Board[1] + state.Board[2] == sum
+                || state.Board[3] + state.Board[4] + state.Board[5] == sum
+                || state.Board[6] + state.Board[7] + state.Board[8] == sum
+                || state.Board[0] + state.Board[3] + state.Board[6] == sum
+                || state.Board[1] + state.Board[4] + state.Board[7] == sum
+                || state.Board[2] + state.Board[5] + state.Board[8] == sum
+                || state.Board[0] + state.Board[4] + state.Board[8] == sum
+                || state.Board[2] + state.Board[4] + state.Board[6] == sum;
         }
     }
 }
--- Chapter04/ActorTicTacToeApplication/Player/Player.cs
+++ Chapter04/ActorTicTacToeApplication/Player/Player.cs
@@ -7,6 +7,7 @@ using Microsoft.ServiceFabric.Actors;
 using Microsoft.ServiceFabric.Actors.Runtime;
 using Microsoft.ServiceFabric.Actors.Client;
 using Player.Interfaces;
+using Game.Interfaces;
 
 namespace Player
 {
@@ -18,44 +19,19 @@ namespace Player
     ///  - Volatile: State is kept in memory only and replicated.
     ///  - None: State is kept in memory only and not replicated.
     /// </remarks>
-    [StatePersistence(StatePersistence.Persisted)]
+    [StatePersistence(StatePersistence.None)]
     internal class Player : Actor, IPlayer
     {
-        /// <summary>
-        /// This method is called whenever an actor is activated.
-        /// An actor is activated the first time any of its methods are invoked.
-        /// </summary>
-        protected override Task OnActivateAsync()
+        public Task<bool> JoinGameAsync(ActorId gameId, string playerName)
         {
-            ActorEventSource.Current.ActorMessage(this, "Actor activated.");
-
-            // The StateManager is this actor's private state store.
-            // Data stored in the StateManager will be replicated for high-availability for actors that use volatile or persisted state storage.
-            // Any serializable object can be saved in the StateManager.
-            // For more information, see http://aka.ms/servicefabricactorsstateserialization
-
-            return this.StateManager.TryAddStateAsync("count", 0);
+            var game = ActorProxy.Create<IGame>(gameId, "fabric:/ActorTicTacToeApplication");
+            return game.JoinGameAsync(this.Id.GetLongId(), playerName);
         }
 
-        /// <summary>
-        /// TODO: Replace with your own actor method.
-        /// </summary>
-        /// <returns></returns>
-        Task<int> IPlayer.GetCountAsync()
+        public Task<bool> MakeMoveAsync(ActorId gameId, int x, int y)
         {
-            return this.StateManager.GetStateAsync<int>("count");
-        }
-
-        /// <summary>
-        /// TODO: Replace with your own actor method.
-        /// </summary>
-        /// <param name="count"></param>
-        /// <returns></returns>
-        Task IPlayer.SetCountAsync(int count)
-        {
-            // Requests are not guaranteed to be processed in order nor at most once.
-            // The update function here verifies that the incoming count is greater than the current count to preserve order.
-            return this.StateManager.AddOrUpdateStateAsync("count", count, (key, value) => count > value ? count : value);
+            var game = ActorProxy.Create<IGame>(gameId, "fabric:/ActorTicTacToeApplication");
+            return game.MakeMoveAsync(this.Id.GetLongId(), x, y);
         }
     }
 }
