43c36da8c089adcd67c1ebbcf0aab276a52a8d22
tsushi <tsushi@DESKTOP-MC5LH8Q>
2016-08-15 23:03:12 +0900

    Writing the TicTacToe tests

---
+++ Chapter04/ActorTicTacToeApplication/TicTacToeTests/GameTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using Game.Interfaces;
+
+namespace TicTacToeTests
+{
+    [TestClass]
+    public class GameTests
+    {
+        [TestMethod]
+        public void APlayerCanOnlyJoinOnce()
+        {
+            var gameId = ActorId.CreateRandom();
+            var game = ActorProxy.Create<IGame>(gameId, "fabric:/ActorTicTacToeApplication");
+            var result1 = game.JoinGameAsync(1L, "Player 1").Result;
+            var result2 = game.JoinGameAsync(1L, "Player 1").Result;
+            Assert.AreEqual(false, result1 & result2);
+        }
+
+        [TestMethod]
+        public void GameCanBeStartedWith2Player()
+        {
+            var gameId = ActorId.CreateRandom();
+            var game = ActorProxy.Create<IGame>(gameId, "fabric:/ActorTicTacToeApplication");
+            game.JoinGameAsync(1L, "Player 1").Wait();
+            var result = game.MakeMoveAsync(1L, 0, 0).Result;
+            Assert.AreEqual(false, result);
+        }
+    }
+}
