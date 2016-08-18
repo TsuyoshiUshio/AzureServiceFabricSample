58098b12ada33ee935c830c1bb6a81493d4a7085
tsushi <tsushi@DESKTOP-MC5LH8Q>
2016-08-18 10:52:41 +0900

    Adding DeviceRegistry with IoT hub

---
+++ Chapter13/SensorAggregationApplication/DeviceRegistry/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+
+namespace DeviceRegistry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            RegistryManager registryManager = RegistryManager.CreateFromConnectionString("HostName=iote2e.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=Bsp4+D5at3lTacsNaZPvx0FhVvrdDa8LGFzKS/B6zzQ=");
+            string deviceId = "DemoDevice";
+            var device = registryManager.GetDeviceAsync(deviceId).Result;
+            if (device == null)
+            {
+                device = registryManager.AddDeviceAsync(new Device(deviceId)).Result;
+            }
+            Console.WriteLine("Device Key: " + device.Authentication.SymmetricKey.PrimaryKey);
+            Console.ReadKey();
+        }
+    }
+}
